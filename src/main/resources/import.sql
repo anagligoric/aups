598140324f67e051d2fe169046c5f7b8feb36ac6
Jovana Savic <jovanasavic996@gmail.com>
2022-07-06 19:21:47 +0200

    LokacijaController added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,9 @@ INSERT INTO role (id, ime) VALUES (2, 'ROLE_USER');
 
 INSERT INTO alat (id, naziv) VALUES (1, 'alat1');
 INSERT INTO alat (id, naziv) VALUES (2, 'alat2');
+
+INSERT INTO lokacija (id, grad, ulica, broj) VALUES (1, 'grad1', 'ulica1', 5);
+INSERT INTO lokacija (id, grad, ulica, broj) VALUES (2, 'grad2', 'ulica2', 3);
+INSERT INTO lokacija (id, grad, ulica, broj) VALUES (3, 'grad3', 'ulica3', 17);
+
+
