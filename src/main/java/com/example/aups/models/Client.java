c7bd9aa2a976a0e572c495f9392224bafb6110f4
Valcon JS <valcon.js@abports.co.uk>
2023-12-06 03:01:06 +0100

    Location deleted, client model updated

--- src/main/java/com/example/aups/models/Client.java
+++ src/main/java/com/example/aups/models/Client.java
@@ -12,21 +12,22 @@ public class Client {
     private String firstName;
     private String surname;
     private String phoneNumber;
-
-    @OneToMany(mappedBy = "client")
-    private Set<Location> locations;
-
-
+    private String city;
+    private String street;
+    private Integer number;
     @OneToMany(mappedBy = "client")
     private Set<Job> jobs;
 
     public Client() {}
 
-    public Client(Long id, String firstName, String surname, String phoneNumber) {
+    public Client(Long id, String firstName, String surname, String phoneNumber, String city, String street, Integer number) {
         this.id = id;
         this.firstName = firstName;
         this.surname = surname;
         this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.street = street;
+        this.number = number;
     }
 
     public Long getId() {
@@ -61,19 +62,35 @@ public class Client {
         this.phoneNumber = phoneNumber;
     }
 
-    public Set<Location> getLocations() {
-        return locations;
-    }
-
-    public void setLocations(Set<Location> locations) {
-        this.locations = locations;
-    }
-
     public Set<Job> getJobs() {
         return jobs;
     }
 
     public void setJobs(Set<Job> poslovi) {
         this.jobs = poslovi;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
     }
 }
